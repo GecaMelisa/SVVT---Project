caa759db84bedd97d3baceac03464a23322156c3
GecaMelisa <melisa.geca@stu.ibu.edu.ba>
2023-12-28 19:14:57 +0100

    My Profile Test - fixed

--- tests/myProfile.test.ts
+++ tests/myProfile.test.ts
@@ -4,12 +4,14 @@ import { readFileSync } from "fs";
 import * as path from "path";
 import { HomePage } from '../core/page-objects/home-page';
 import { MyProfile} from "../core/page-objects/myProfile-page";
+import { LoginPage } from "../core/page-objects/login-page";
 
 const dataFilePath = path.resolve(__dirname, "../core/data/data.json");
 const testData = JSON.parse(readFileSync(dataFilePath, "utf8"));
 
 let driver: WebDriver;
 let homePage: HomePage;
+let loginPage: LoginPage;
 let myProfile: MyProfile;
 
 
@@ -17,6 +19,7 @@ let myProfile: MyProfile;
 beforeAll(async () => {
     driver = await createDriver(testData.url.home_page);
     homePage = new HomePage(driver);
+    loginPage = new LoginPage(driver);
     myProfile = new MyProfile(driver);
    
 }, 10000);
@@ -24,9 +27,11 @@ beforeAll(async () => {
 test('MyProfile', async () => {
     await homePage.agree();
     await homePage.clickLogin();
+    await loginPage.enterEmail();
+    await loginPage.enterPassword();
+    await loginPage.clickLogin();
     await myProfile.clickOnMyProfile();
     await myProfile.findMyName();
-    await myProfile.locateActiveAd();
     await myProfile.findActiveAd();
    
     
